6fa1d01a4da3891b4b5b29bb096598570a09fc58
stephane brossier <stephane@ning.com>
2013-11-15 21:12:30 -0800

    Add pause/resume tests with ADD_ON

--- killbill-integration-tests/core/test_pause_resume.rb
+++ killbill-integration-tests/core/test_pause_resume.rb
@@ -88,6 +88,122 @@ module KillBillIntegrationTests
 
     end
 
+    def test_with_ao
+
+      # First invoice  01/08/2013 -> 31/08/2013 ($0) => BCD = 31
+      bp = create_entitlement_base(@account.account_id, 'Sports', 'MONTHLY', 'DEFAULT', @user, @options)
+      check_entitlement(bp, 'Sports', 'BASE', 'MONTHLY', 'DEFAULT', DEFAULT_KB_INIT_DATE, nil)
+
+
+      # Second invoice
+      # Move clock  (BP out of trial)
+      kb_clock_add_days(30, nil, @options) # 31/08/2013
+
+      all_invoices = @account.invoices(true, @options)
+      assert_equal(all_invoices.size, 2)
+      sort_invoices!(all_invoices)
+      second_invoice = all_invoices[1]
+      check_invoice(second_invoice, 500.00, 'USD', "2013-08-31", 0)
+      check_invoice_item(second_invoice.items[0], second_invoice.invoice_id, 500.00, 'USD', 'RECURRING', 'sports-monthly', 'sports-monthly-evergreen', '2013-08-31', '2013-09-30')
+
+
+      # Move clock  and create ao
+      # Third invoice : 2/09/2013 -> 30/09/2013
+      kb_clock_add_days(2, nil, @options) # 2/09/2013
+      ao1 = create_entitlement_ao(bp.bundle_id, 'OilSlick', 'MONTHLY', 'DEFAULT', @user, @options)
+      check_entitlement(ao1, 'OilSlick', 'ADD_ON', 'MONTHLY', 'DEFAULT', "2013-09-02", nil)
+
+      sleep 3; # There is no sync on pause_bundle to wait for invoice generation
+      all_invoices = @account.invoices(true, @options)
+      assert_equal(3, all_invoices.size)
+      sort_invoices!(all_invoices)
+      third_invoice = all_invoices[2]
+      check_invoice(third_invoice, 7.18, 'USD', "2013-09-02", 0)
+      assert_equal(1, third_invoice.items.size)
+      check_invoice_item(third_invoice.items[0], third_invoice.invoice_id, 7.18, 'USD', 'RECURRING', 'oilslick-monthly', 'oilslick-monthly-evergreen', '2013-09-02', '2013-09-30')
+
+
+      # Move clock  and pause bundle
+      kb_clock_add_days(3, nil, @options) # 5/09/2013
+      pause_bundle(bp.bundle_id, nil, @user, @options)
+
+
+      # Verify last invoice was adjusted
+      sleep 3; # There is no sync on pause_bundle to wait for invoice generation
+      all_invoices = @account.invoices(true, @options)
+      assert_equal(3, all_invoices.size)
+      sort_invoices!(all_invoices)
+
+      second_invoice = all_invoices[1]
+      check_invoice(second_invoice, 83.35, 'USD', "2013-08-31", 0)
+      assert_equal(3, second_invoice.items.size)
+      check_invoice_item(second_invoice.items[0], second_invoice.invoice_id, 500.00, 'USD', 'RECURRING', 'sports-monthly', 'sports-monthly-evergreen', '2013-08-31', '2013-09-30')
+      check_invoice_item(second_invoice.items[1], second_invoice.invoice_id, -416.65, 'USD', 'REPAIR_ADJ', nil, nil, '2013-09-05', '2013-09-30')
+      check_invoice_item(second_invoice.items[2], second_invoice.invoice_id, 416.65, 'USD', 'CBA_ADJ', nil, nil, '2013-09-05', '2013-09-05')
+
+      third_invoice = all_invoices[2]
+      check_invoice(third_invoice, 0.77, 'USD', "2013-09-02", 0)
+      assert_equal(3, third_invoice.items.size)
+      check_invoice_item(third_invoice.items[0], third_invoice.invoice_id, 7.18, 'USD', 'RECURRING', 'oilslick-monthly', 'oilslick-monthly-evergreen', '2013-09-02', '2013-09-30')
+      check_invoice_item(third_invoice.items[1], third_invoice.invoice_id, -6.41, 'USD', 'REPAIR_ADJ', nil, nil, '2013-09-05', '2013-09-30')
+      check_invoice_item(third_invoice.items[2], third_invoice.invoice_id, 6.41, 'USD', 'CBA_ADJ', nil, nil, '2013-09-05', '2013-09-05')
+
+      # Move clock
+      kb_clock_add_days(5, nil, @options) # 10/09/2013
+      resume_bundle(bp.bundle_id, nil, @user, @options)
+
+      # Verify last invoice was adjusted
+      sleep 3; # There is no sync on resume_bundle to wait for invoice generation
+      all_invoices = @account.invoices(true, @options)
+      assert_equal(4, all_invoices.size)
+      sort_invoices!(all_invoices)
+      fourth_invoice = all_invoices[3]
+      check_invoice(fourth_invoice, 327.730, 'USD', "2013-09-10", 0)
+      assert_equal(3, fourth_invoice.items.size)
+      check_invoice_item(get_specific_invoice_item(fourth_invoice.items, 'RECURRING', 'oilslick-monthly-evergreen'), fourth_invoice.invoice_id, 5.13, 'USD', 'RECURRING', 'oilslick-monthly', 'oilslick-monthly-evergreen', '2013-09-10', '2013-09-30')
+      check_invoice_item(get_specific_invoice_item(fourth_invoice.items, 'RECURRING', 'sports-monthly-evergreen'), fourth_invoice.invoice_id, 322.60, 'USD', 'RECURRING', 'sports-monthly', 'sports-monthly-evergreen', '2013-09-10', '2013-09-30')
+      check_invoice_item(get_specific_invoice_item(fourth_invoice.items, 'CBA_ADJ', nil), fourth_invoice.invoice_id, -327.73, 'USD', 'CBA_ADJ', nil, nil, '2013-09-10', '2013-09-10')
+
+
+      subscriptions = get_subscriptions(bp.bundle_id, @options)
+      assert_not_nil(subscriptions)
+      assert_equal(subscriptions.size, 2)
+
+      bp = subscriptions.find { |s| s.subscription_id == bp.subscription_id }
+      check_subscription(bp, 'Sports', 'BASE', 'MONTHLY', 'DEFAULT', "2013-08-01", nil, "2013-08-01", nil)
+      check_events(bp.events, [{:type => "START_ENTITLEMENT", :date => "2013-08-01"},
+                               {:type => "START_BILLING", :date => "2013-08-01"},
+                               {:type => "PHASE", :date => "2013-08-31"},
+                               {:type => "PAUSE_ENTITLEMENT", :date => "2013-09-05"},
+                               {:type => "PAUSE_BILLING", :date => "2013-09-05"},
+                               {:type => "RESUME_ENTITLEMENT", :date => "2013-09-10"},
+                               {:type => "RESUME_BILLING", :date => "2013-09-10"}])
+
+
+      # No DISCOUNT phase as we started he subscription more than a month after BP and this is bundle aligned.
+      ao1 = subscriptions.find { |s| s.subscription_id == ao1.subscription_id }
+      check_subscription(ao1, 'OilSlick', 'ADD_ON', 'MONTHLY', 'DEFAULT', "2013-09-02", nil, "2013-09-02", nil)
+      check_events(ao1.events, [{:type => "START_ENTITLEMENT", :date => "2013-09-02"},
+                                {:type => "START_BILLING", :date => "2013-09-02"},
+                                {:type => "PAUSE_ENTITLEMENT", :date => "2013-09-05"},
+                                {:type => "PAUSE_BILLING", :date => "2013-09-05"},
+                                {:type => "RESUME_ENTITLEMENT", :date => "2013-09-10"},
+                                {:type => "RESUME_BILLING", :date => "2013-09-10"}])
+
+
+    end
+
+    private
+
+    def get_specific_invoice_item(items, type, phase_name)
+      items.each do |i|
+        if i.phase_name == phase_name && i.item_type == type
+          return i
+        end
+      end
+      nil
+    end
+
   end
 end
 
