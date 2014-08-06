a21e3812179baeba58920062a6efcf4cc24bd5e0
stephane brossier <sbrossier@groupon.com>
2014-08-06 10:58:24 -0700

    Update ruby client lib and add debug messages in assertions

--- killbill-integration-tests/mixin-checkers/invoice_checker.rb
+++ killbill-integration-tests/mixin-checkers/invoice_checker.rb
@@ -6,21 +6,24 @@ module KillBillIntegrationTests
     include CheckerBase
 
     def check_invoice_no_balance(i, amount, currency, invoice_date)
+      msg = "invoice #{i.invoice_id}"
+
       assert_not_nil(i.invoice_number)
-      assert_equal(amount, i.amount)
-      assert_equal(currency, i.currency)
-      assert_equal(invoice_date, i.invoice_date)
+      assert_equal(amount, i.amount,  msg)
+      assert_equal(currency, i.currency, msg)
+      assert_equal(invoice_date, i.invoice_date, msg)
     end
 
     def check_invoice_item(ii, invoice_id, amount, currency, item_type, plan_name, phase_name, start_date, end_date)
-      assert_equal(amount, ii.amount)
-      assert_equal(invoice_id, ii.invoice_id)
-      assert_equal(currency, ii.currency)
-      assert_equal(item_type, ii.item_type)
-      assert_equal(plan_name, ii.plan_name)
-      assert_equal(phase_name, ii.phase_name)
-      assert_equal(start_date, ii.start_date)
-      assert_equal(end_date, ii.end_date)
+      msg = "invoice_item #{ii.invoice_item_id}"
+      assert_equal(amount, ii.amount, msg)
+      assert_equal(invoice_id, ii.invoice_id, msg)
+      assert_equal(currency, ii.currency, msg)
+      assert_equal(item_type, ii.item_type, msg)
+      assert_equal(plan_name, ii.plan_name, msg)
+      assert_equal(phase_name, ii.phase_name, msg)
+      assert_equal(start_date, ii.start_date, msg)
+      assert_equal(end_date, ii.end_date, msg)
     end
 
   end
