66aad2052144bf80020dc2edfc9a9fa7b4c0befa
stephane brossier <sbrossier@groupon.com>
2015-04-25 10:45:38 -0700

    Add consumable in arrear usage tests (based on a cloud model)

---
+++ killbill-integration-tests/core/usage/test_cloud.rb
@@ -0,0 +1,178 @@
+$LOAD_PATH.unshift File.expand_path('../../..', __FILE__)
+
+require 'test_base'
+
+module KillBillIntegrationTests
+
+  class TestCloud < Base
+
+    def setup
+      setup_base
+
+      catalog_file_xml = get_resource_as_string("usage/cloud.xml")
+      KillBillClient::Model::Catalog.upload_tenant_catalog(catalog_file_xml, @user, 'New Catalog Version', 'Upload catalog for tenant', @options)
+
+      @account = create_account(@user, @options)
+    end
+
+    def teardown
+      teardown_base
+      end
+
+    NB_USAGE_TYPES = 3
+    MAX_SERVERS_PER_TYPE = 10
+    USAGE_MIN = 0
+    USAGE_MAX = 24 * MAX_SERVERS_PER_TYPE
+
+
+    def test_demo_with_multiple_units
+
+
+      #
+      # Step 1. Create a subscription associated with the account `@account` (or Project in Packet's terminology)
+      #
+      bp = create_entitlement_base(@account.account_id, 'Server', 'MONTHLY', 'DEFAULT', @user, @options)
+      # Test waits synchronously until the first invoice was generated ($0 invoice since no usage was recorded yet)
+      wait_for_expected_clause(1, @account, @options, &@proc_account_invoices_nb)
+
+
+      # Step 2. Record usage: For each usage type, we generate random usage value per day, each point between [USAGE_MIN, USAGE_MAX)
+      usage_input = []
+      expected_dollar_amount_total = 0
+      expected_dollar_amount_per_unit = {}
+      NB_USAGE_TYPES.times do |i|
+
+        raw_usage = generate_random_usage_values(8, 2013, USAGE_MIN, USAGE_MAX)
+        expected_usage_unit_amount = raw_usage.inject(0) { |sum, e| sum += e }
+        # Price for unit 1 is $1, price for unit 2 is $2, ...
+        expected_dollar_amount_per_unit[i] = expected_usage_unit_amount * (i + 1)
+
+        expected_dollar_amount_total += expected_dollar_amount_per_unit[i]
+
+        usage_input << {:unit_type => "server-hourly-type-#{i + 1}",
+                        :usage_records => generate_usage_for_each_day(8, 2013, raw_usage)
+        }
+
+      end
+      #
+      # Make the call to record the usage (POST /1.0/kb/usages).
+      #
+      # We make one bulk call to record all the points for all usage types in the month, but we could
+      # also make one call per day, and/or one call per unit type per day...
+      #
+      record_usage(bp.subscription_id, usage_input, @user, @options)
+
+
+      #
+      # Step 3. Move the clock to the beginning of the next month '2013-09-01' to trigger the first invoice with usage items
+      #
+      kb_clock_add_days(31, nil, @options)
+      wait_for_expected_clause(2, @account, @options, &@proc_account_invoices_nb)
+
+      #
+      # Verify we see an invoice with:
+      # * 3 usage items (one for each type) for the previous month (in arrear usage billing)
+      # * 1 recurring item of $0 for the next month
+      #
+      all_invoices = @account.invoices(true, @options)
+      sort_invoices!(all_invoices)
+      assert_equal(2, all_invoices.size)
+      last_invoice = all_invoices[1]
+      check_invoice_no_balance(last_invoice, expected_dollar_amount_total, 'USD', '2013-09-01')
+      check_invoice_item(last_invoice.items[0], last_invoice.invoice_id, 0.0, 'USD', 'RECURRING', 'server-monthly', 'server-monthly-evergreen', '2013-09-01', '2013-10-01')
+
+      check_usage_invoice_item(last_invoice.items[1], last_invoice.invoice_id, expected_dollar_amount_per_unit[2], 'USD', 'USAGE', 'server-monthly', 'server-monthly-evergreen', 'server-monthly-usage-type-3', '2013-08-01', '2013-09-01')
+      check_usage_invoice_item(last_invoice.items[2], last_invoice.invoice_id, expected_dollar_amount_per_unit[1], 'USD', 'USAGE', 'server-monthly', 'server-monthly-evergreen', 'server-monthly-usage-type-2', '2013-08-01', '2013-09-01')
+        check_usage_invoice_item(last_invoice.items[3], last_invoice.invoice_id, expected_dollar_amount_per_unit[0], 'USD', 'USAGE', 'server-monthly', 'server-monthly-evergreen', 'server-monthly-usage-type-1', '2013-08-01', '2013-09-01')
+
+    end
+
+
+    def test_with_multiple_units
+
+
+      bps = []
+      nb_subscriptions = 100
+      nb_subscriptions.times do |i|
+        bp = create_entitlement_base(@account.account_id, 'Server', 'MONTHLY', 'DEFAULT', @user, @options)
+        wait_for_expected_clause(i + 1, @account, @options, &@proc_account_invoices_nb)
+        bps << bp
+      end
+
+
+
+      expected_usage_unit_amount_for_bps = {}
+      bps.each do |bp|
+
+
+        usage_input = []
+
+        bp_expected_usage_unit_amount = 0
+
+        3.times do |i|
+
+          raw_usage = generate_random_usage_values(8, 2013, 0, 178)
+          expected_usage_unit_amount = raw_usage.inject(0) { |sum, e| sum += e}
+          expected_usage_unit_amount = expected_usage_unit_amount * (i + 1)
+
+          bp_expected_usage_unit_amount += expected_usage_unit_amount
+
+          usage_input << {:unit_type => "server-hourly-type-#{i + 1}",
+                          :usage_records => generate_usage_for_each_day(8, 2013, raw_usage)
+          }
+
+        end
+
+        expected_usage_unit_amount_for_bps[bp.subscription_id] = bp_expected_usage_unit_amount
+        record_usage(bp.subscription_id, usage_input, @user, @options)
+
+      end
+
+      all_expected_usage_unit_amount = expected_usage_unit_amount_for_bps.values.inject(0) { |sum, e| sum += e }
+
+      kb_clock_add_days(31, nil, @options)
+      wait_for_expected_clause(nb_subscriptions + 1, @account, @options, &@proc_account_invoices_nb)
+
+      all_invoices = @account.invoices(true, @options)
+
+      puts "all_invoices  = #{all_invoices.size}"
+
+      sort_invoices!(all_invoices)
+      assert_equal(nb_subscriptions + 1, all_invoices.size)
+      last_invoice = all_invoices[nb_subscriptions]
+      check_invoice_no_balance(last_invoice, all_expected_usage_unit_amount, 'USD', '2013-09-01')
+      check_invoice_item(last_invoice.items[0], last_invoice.invoice_id, 0.0, 'USD', 'RECURRING', 'server-monthly', 'server-monthly-evergreen', '2013-09-01', '2013-10-01')
+    end
+
+    
+    private
+
+    COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def days_in_month(month, year)
+      return 29 if month == 2 && Date.gregorian_leap?(year)
+      COMMON_YEAR_DAYS_IN_MONTH[month]
+    end
+
+    def generate_random_usage_values(month, year, min_included, max_excluded)
+      res = []
+      nb_values = days_in_month(month, year)
+      nb_values.times { |i| res << rand(min_included...max_excluded) }
+      res
+    end
+
+    def generate_usage_for_each_day(month, year, raw_usage)
+      res= []
+      raw_usage.inject(1) do |day, e|
+        new_value = {}
+        new_value[:record_date] = day < 10 ? "#{year.to_s}-#{month.to_s}-0#{day}" : "#{year.to_s}-#{month.to_s}-#{day}"
+        new_value[:amount] = e
+        res << new_value
+        day +=1
+      end
+      res
+    end
+
+
+  end
+end
---
+++ killbill-integration-tests/load/usage_cloud_load.rb
@@ -0,0 +1,217 @@
+$LOAD_PATH.unshift File.expand_path('../..', __FILE__)
+
+require 'test_base'
+
+module KillBillIntegrationTests
+
+  class TestCloud < Base
+
+    def setup
+      setup_base
+      catalog_file_xml = get_resource_as_string("usage/cloud.xml")
+      KillBillClient::Model::Catalog.upload_tenant_catalog(catalog_file_xml, @user, 'New Catalog Version', 'Upload catalog for tenant', @options)
+    end
+
+    def teardown
+      teardown_base
+      end
+
+    NB_USAGE_TYPES = 3
+    MAX_SERVERS_PER_TYPE = 10
+    USAGE_MIN = 0
+    USAGE_MAX = 24 * MAX_SERVERS_PER_TYPE
+
+    NB_ACCOUNTS_PER_DAY = 3
+    NB_SUBSCRIPTIONS_PER_ACCOUNT = 3
+
+
+    def test_with_accounts_subscriptions_units
+
+      nb_account_per_day = NB_ACCOUNTS_PER_DAY
+      nb_subscriptions_per_account = NB_SUBSCRIPTIONS_PER_ACCOUNT
+
+      #
+      # Initialize by creating `nb_account_per_day`  with each `nb_subscriptions_per_account`
+      # As we create the accounts we add random usage each day for subscriptions created and keep track of the usage
+      #
+      all_accounts = initialize_all_account_with_usage_data(nb_account_per_day, nb_subscriptions_per_account)
+
+      #
+      # At run through a few months and each day:
+      # 1. verify the new invoices generated (By checking $ amount matches what we created)
+      # 2. keep adding usage for all subscriptions on each day
+      #
+      prev_nb_day_in_month = 31
+      gen_month_year_entries(9, 2013, 12).each_with_index do |month_year, idx|
+
+        month = month_year[0]
+        year = month_year[1]
+
+        cur_nb_day_in_month = days_in_month(month, year)
+        cur_nb_day_in_month.times do |zero_based_day|
+
+          kb_clock_add_days(1, nil, @options)
+
+          day = zero_based_day + 1
+          date = format_date(day, month, year)
+
+          puts "*** [#{date}]: NEXT ITERATION..."
+
+          verify_invoices_for_account_range_and_reset_usage(all_accounts, zero_based_day, 0, month, year, nb_account_per_day, nb_subscriptions_per_account, idx)
+
+          # There is a little bit of complexity to take care of the shorter months and make sure that for instance accounts created on the 31 will correctly be checked
+          # in months with 30 days and their counter will be reset to 0 , so next month, the invoice check still works!
+          #
+          if prev_nb_day_in_month && cur_nb_day_in_month == zero_based_day + 1 && cur_nb_day_in_month < prev_nb_day_in_month
+            (prev_nb_day_in_month-cur_nb_day_in_month).times do |catch_up_idx|
+              verify_invoices_for_account_range_and_reset_usage(all_accounts, zero_based_day, (catch_up_idx + 1), month, year, nb_account_per_day, nb_subscriptions_per_account, idx)
+            end
+          end
+
+          add_usage_to_existing_subscriptions(all_accounts, year, month, zero_based_day + 1)
+
+        end
+
+        prev_nb_day_in_month = cur_nb_day_in_month
+      end
+    end
+
+
+
+    def add_usage_to_existing_subscriptions(all_accounts, year, month, day)
+
+      all_accounts.each do |account_entry|
+
+        additional_per_account_usage = 0
+
+        # For each subscription record some rabndom usage data
+        account_entry[:bps].each do |bp|
+
+          usage_input = []
+          NB_USAGE_TYPES.times do |i|
+
+            # Generate random amount of usage data
+            raw_usage_amount = rand(USAGE_MIN...USAGE_MAX)
+            # To compute the price we know that unit 1 => $1, unit 2 => $2, unit 3 => $3, hence the multiplication with (i + 1)
+            additional_per_account_usage += raw_usage_amount * (i + 1)
+            usage_input << {
+                :unit_type => "server-hourly-type-#{i + 1}",
+                :usage_records => [{:record_date => format_date(day, month, year), :amount => raw_usage_amount}]
+            }
+          end
+          # Record usage on that day for that subscription (all 3 units in one call)
+          record_usage(bp.subscription_id, usage_input, @user, @options)
+        end
+        # Keep track of usage so we can't verify the price of the invoice generated
+        account_entry[:usage] = account_entry[:usage].nil? ? additional_per_account_usage : account_entry[:usage] + additional_per_account_usage
+      end
+    end
+
+
+
+    def create_new_account_with_subscriptions(nb_accounts, nb_subscriptions_per_account)
+
+      accounts = []
+      # Create all he accounts for that day
+      nb_accounts.times do |nb|
+        account = create_account(@user, @options)
+        bps = []
+        # For each account create the subscriptions
+        nb_subscriptions_per_account.times do |i|
+          bp = create_entitlement_base(account.account_id, 'Server', 'MONTHLY', 'DEFAULT', @user, @options)
+          # We expect one invoice per subscription created
+          wait_for_expected_clause(i + 1, account, @options, &@proc_account_invoices_nb)
+          bps << bp
+        end
+        # Keep track of the created accounts along with its subscriptions
+        accounts << { :account => account, :bps => bps}
+      end
+      accounts
+    end
+
+    def initialize_all_account_with_usage_data(nb_account_per_day, nb_subscriptions_per_account)
+
+      # Clock has been initialized at 2013-08-01
+      year = 2013
+      month = 8
+
+      all_accounts = []
+      # For each day of the month we create the accounts/subscriptions and start inserting usage data
+      days_in_month(month, year).times do |zero_based_day|
+
+        day = zero_based_day + 1
+        date = format_date(day, month, year)
+        puts "*** [#{date}]: CREATE #{nb_account_per_day} ACCOUNT(S) WITH #{nb_subscriptions_per_account} SUBSCRIPTION(S)"
+
+        all_accounts << create_new_account_with_subscriptions(nb_account_per_day, nb_subscriptions_per_account)
+        all_accounts.flatten!
+
+        add_usage_to_existing_subscriptions(all_accounts, year, month, (zero_based_day + 1))
+
+        puts "*** [#{date}]: ADDING USAGE TO SUBSCRIPTIONS..."
+        kb_clock_add_days(1, nil, @options) if zero_based_day < 30
+      end
+      all_accounts
+    end
+
+    def verify_invoices_for_account_range_and_reset_usage(all_accounts, zero_based_day, catch_up_end_of_month_idx, month, year, nb_account_per_day, nb_subscriptions_per_account, month_idx)
+
+      day = zero_based_day + 1
+      date = format_date(day, month, year)
+
+      account_idx = zero_based_day + catch_up_end_of_month_idx
+      #
+      # For each day, we should have as many new invoices as we created accounts on that day, so we extract the range of accounts that match that day
+      # (along with catch up days that don't exist in that specific months as indicated by catch_up_end_of_month_idx)
+      #
+      all_accounts[account_idx*nb_account_per_day..((account_idx+1)*nb_account_per_day)-1].each do |account_entry|
+        puts "*** [#{date}]: WAITING FOR INVOICES FOR ACCOUNT #{account_entry[:account].account_id}"
+
+        # Verify new invoice got created
+        expected_number_invoices = nb_subscriptions_per_account + (month_idx + 1)
+        wait_for_expected_clause(expected_number_invoices, account_entry[:account], @options, &@proc_account_invoices_nb)
+
+        all_invoices = account_entry[:account].invoices(true, @options)
+        sort_invoices!(all_invoices)
+        assert_equal(expected_number_invoices, all_invoices.size)
+        last_invoice = all_invoices[expected_number_invoices - 1]
+
+        # Verify balance in that invoice based on what we computed internally
+        check_invoice_no_balance(last_invoice, account_entry[:usage], 'USD', format_date(day, month, year))
+          # Reset entry to prepare for accrual in the next rolling month
+        account_entry[:usage] = 0
+      end
+
+    end
+
+
+    private
+
+    COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def days_in_month(month, year)
+      return 29 if month == 2 && Date.gregorian_leap?(year)
+      COMMON_YEAR_DAYS_IN_MONTH[month]
+    end
+
+    def format_date(day, month, year)
+      day_str = day < 10 ? "0#{day}" : "#{day}"
+      month_str = month < 10 ? "0#{month}" : "#{month}"
+      "#{year.to_s}-#{month_str}-#{day_str}"
+    end
+
+
+    def gen_month_year_entries(init_month, init_year, nb_months)
+      result = []
+      cur_month = init_month
+      cur_year = init_year
+      nb_months.times do |idx|
+        result << [cur_month, cur_year]
+        cur_month = cur_month < 12 ? cur_month + 1 : 1
+        cur_year = cur_month == 1 ? cur_year + 1 : cur_year
+      end
+      result
+    end
+
+  end
+end
