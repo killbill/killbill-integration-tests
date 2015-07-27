9e5731080acb15f56f4895ce6b4fb349ff04ddf3
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-07-27 08:54:05 -0700

    jmeter: remove transaction block
    This confuses flood.io stats.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- killbill-integration-tests/load/jmeter/payment.rb
+++ killbill-integration-tests/load/jmeter/payment.rb
@@ -16,79 +16,77 @@ scenario = test do
 
   threads count: NB_THREADS.to_i, rampup: 30, duration: DURATION.to_i, on_sample_error: 'startnextloop' do
 
-    transaction 'Create an account and trigger a payment' do
-      counter = '${__counter(false)}'
+    counter = '${__counter(false)}'
 
+    #
+    # Create account
+    #
+
+    first, last = "John #{counter}", "Bill"
+    account = {
+        name: "#{first} #{last}",
+        email: "johny#{counter}-#{START_TIME}@killbill.io",
+        currency: 'USD'
+    }
+    post name: :'Create Account',
+         url: KB_ACCOUNTS_URL,
+         raw_body: account.to_json,
+         headers: true do
+      assert equals: 201, test_field: 'Assertion.response_code'
+      extract name: 'account_id',
+              regex: LOCATION_ID_REGEX,
+              headers: true
+    end
+
+    exists 'account_id' do
       #
-      # Create account
+      # Add default payment method
       #
 
-      first, last = "John #{counter}", "Bill"
-      account = {
-          name: "#{first} #{last}",
-          email: "johny#{counter}-#{START_TIME}@killbill.io",
-          currency: 'USD'
+      payment_method_body_body = {
+          pluginName: PLUGIN,
+          pluginInfo: {
+              properties: [
+                  {key: 'email', value: 'tom@killbill.io'},
+                  {key: 'description', value: START_TIME},
+                  {key: 'ccFirstName', value: first},
+                  {key: 'ccLastName', value: last},
+                  {key: 'address1', value: '5th street'},
+                  {key: 'city', value: 'San Francisco'},
+                  {key: 'zip', value: '94111'},
+                  {key: 'state', value: 'CA'},
+                  {key: 'country', value: 'US'},
+                  {key: 'ccNumber', value: '4242424242424242'},
+                  {key: 'ccExpirationYear', value: '2020'},
+                  {key: 'ccExpirationMonth', value: '10'},
+              ]
+          }
       }
-      post name: :'Create Account',
-           url: KB_ACCOUNTS_URL,
-           raw_body: account.to_json,
-           headers: true do
+      post name: :'Add default Payment Method',
+           url: "#{KB_ACCOUNTS_URL}/${account_id}/paymentMethods?isDefault=true&#{PLUGIN_PROPERTIES}",
+           raw_path: true,
+           raw_body: payment_method_body_body.to_json do
         assert equals: 201, test_field: 'Assertion.response_code'
-        extract name: 'account_id',
+        extract name: 'payment_method_id',
                 regex: LOCATION_ID_REGEX,
                 headers: true
       end
 
-      exists 'account_id' do
+      exists 'payment_method_id' do
         #
-        # Add default payment method
+        # Trigger payment
         #
 
-        payment_method_body_body = {
-            pluginName: PLUGIN,
-            pluginInfo: {
-                properties: [
-                    {key: 'email', value: 'tom@killbill.io'},
-                    {key: 'description', value: START_TIME},
-                    {key: 'ccFirstName', value: first},
-                    {key: 'ccLastName', value: last},
-                    {key: 'address1', value: '5th street'},
-                    {key: 'city', value: 'San Francisco'},
-                    {key: 'zip', value: '94111'},
-                    {key: 'state', value: 'CA'},
-                    {key: 'country', value: 'US'},
-                    {key: 'ccNumber', value: '4242424242424242'},
-                    {key: 'ccExpirationYear', value: '2020'},
-                    {key: 'ccExpirationMonth', value: '10'},
-                ]
-            }
+        payment_body = {
+            transactionType: 'PURCHASE',
+            amount: 10,
+            currency: account['currency']
         }
-        post name: :'Add default Payment Method',
-             url: "#{KB_ACCOUNTS_URL}/${account_id}/paymentMethods?isDefault=true&#{PLUGIN_PROPERTIES}",
+        post name: :"Purchase Payment",
+             url: "#{KB_ACCOUNTS_URL}/${account_id}/payments?#{PLUGIN_PROPERTIES}",
              raw_path: true,
-             raw_body: payment_method_body_body.to_json do
+             raw_body: payment_body.to_json do
           assert equals: 201, test_field: 'Assertion.response_code'
-          extract name: 'payment_method_id',
-                  regex: LOCATION_ID_REGEX,
-                  headers: true
-        end
-
-        exists 'payment_method_id' do
-          #
-          # Trigger payment
-          #
-
-          payment_body = {
-              transactionType: 'PURCHASE',
-              amount: 10,
-              currency: account['currency']
-          }
-          post name: :"Purchase Payment",
-               url: "#{KB_ACCOUNTS_URL}/${account_id}/payments?#{PLUGIN_PROPERTIES}",
-               raw_path: true,
-               raw_body: payment_body.to_json do
-            assert equals: 201, test_field: 'Assertion.response_code'
-          end
         end
       end
     end
