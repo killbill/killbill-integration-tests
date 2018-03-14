9446c3a14618fc5bcf252633062a6450d5ed08ba
José Francisco Rodríguez Zayas <jfrodriguez@wovenware.com>
2018-03-14 16:19:10 -0400

    Resolve merge conflict

--- killbill-integration-tests/core/test_invoice_payment.rb
+++ killbill-integration-tests/core/test_invoice_payment.rb
@@ -153,7 +153,7 @@ module KillBillIntegrationTests
       assert_equal(0.0, invoice.balance)
       assert_equal(-50.0, invoice.refund_adj)
     end
-    
+
     def test_get_account_invoice_payments
       # Verify if the returned list is empty
       assert(@account.invoice_payments('NONE', false, false, @options).empty?)
@@ -167,6 +167,41 @@ module KillBillIntegrationTests
       account_invoice_payments = @account.invoice_payments('NONE', false, false, @options)
       assert_equal(7.0, account_invoice_payments[0].purchased_amount)
       assert_equal(5.0, account_invoice_payments[1].purchased_amount)
+    end
+
+    def test_create_chargeback_and_chargeback_reversal
+
+      # Create a charge to account
+      create_charge(@account.account_id, '50.0', 'USD', 'My charge', @user, @options)
+
+      # Create a payment
+      pay_all_unpaid_invoices(@account.account_id, true, '50.0', @user, @options)
+
+      account = get_account(@account.account_id, true, true, @options)
+      payment_id = account.payments(@options).first.payment_id
+
+      # Verify if a new transaction is created and if their type is PURCHASE
+      account_transactions = account.payments(@options).first.transactions
+      assert_equal(1, account_transactions.size)
+      assert_equal('PURCHASE', account_transactions[0].transaction_type)
+
+      # Trigger chargerback
+      chargeback = KillBillClient::Model::InvoicePayment.create_chargeback(payment_id, '50.0', 'USD', nil, @user, nil, nil, @options)
+
+      # Verify if a new transaction is created and if their type is CHARGEBACK
+      account_transactions = account.payments(@options).first.transactions
+      assert_equal(2, account_transactions.size)
+      assert_equal('CHARGEBACK', account_transactions[1].transaction_type)
+
+      # Trigger chargerback reversal
+      transaction_external_key = chargeback.transactions[1].transaction_external_key
+      KillBillClient::Model::InvoicePayment.chargeback_reversal(payment_id, transaction_external_key, nil, @user, nil, nil, @options)
+
+      # Verify if a new transaction is created and if their type is CHARGEBACK
+      account_transactions = account.payments(@options).first.transactions
+      assert_equal(3, account_transactions.size)
+      assert_equal('CHARGEBACK', account_transactions[2].transaction_type)
+
     end
 
   end
