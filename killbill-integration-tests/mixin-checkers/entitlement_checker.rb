064fc5dc935149d7a9bdfe4345601ccceff03d91
Reshma Bidikar <85998496+reshmabidikar@users.noreply.github.com>
2022-05-04 16:13:34 +0530

    Fixed rubocop errors

--- killbill-integration-tests/mixin-checkers/entitlement_checker.rb
+++ killbill-integration-tests/mixin-checkers/entitlement_checker.rb
@@ -11,13 +11,8 @@ module KillBillIntegrationTests
 
     def check_subscription(s, product_name, product_category, billing_period, price_list, start_date, cancelled_date, billing_start_date, billing_end_date, account_time_zone = nil)
       check_entitlement(s, product_name, product_category, billing_period, price_list, start_date, cancelled_date, account_time_zone)
-      if account_time_zone
-        check_with_nil(billing_start_date, (TZInfo::Timezone.get(account_time_zone).utc_to_local(Time.parse(s.billing_start_date)).to_date.to_s if s.billing_start_date))
-        check_with_nil(billing_end_date, (TZInfo::Timezone.get(account_time_zone).utc_to_local(Time.parse(s.billing_end_date)).to_date.to_s if s.billing_end_date))
-      else
-        check_with_nil(billing_start_date, (s.billing_start_date.partition('T')[0] if s.billing_start_date))
-        check_with_nil(billing_end_date, (s.billing_end_date.partition('T')[0] if s.billing_end_date))
-      end
+      assert_equal_dates(billing_start_date, s.billing_start_date, account_time_zone)
+      assert_equal_dates(billing_end_date, s.billing_end_date, account_time_zone)
     end
 
     def check_entitlement(e, product_name, product_category, billing_period, price_list, start_date, cancelled_date, account_time_zone = nil)
@@ -25,13 +20,8 @@ module KillBillIntegrationTests
       check_with_nil(product_category, e.product_category)
       check_with_nil(billing_period, e.billing_period)
       check_with_nil(price_list, e.price_list)
-      if account_time_zone
-        check_with_nil(start_date, (TZInfo::Timezone.get(account_time_zone).utc_to_local(Time.parse(e.start_date)).to_date.to_s if e.start_date))
-        check_with_nil(cancelled_date, (TZInfo::Timezone.get(account_time_zone).utc_to_local(Time.parse(e.cancelled_date)).to_date.to_s if e.cancelled_date))
-      else
-        check_with_nil(start_date, (e.start_date.partition('T')[0] if e.start_date))
-        check_with_nil(cancelled_date, (e.cancelled_date.partition('T')[0] if e.cancelled_date))
-      end
+      assert_equal_dates(start_date, e.start_date, account_time_zone)
+      assert_equal_dates(cancelled_date, e.cancelled_date, account_time_zone)
     end
 
     def check_events(expected, real)
@@ -48,6 +38,16 @@ module KillBillIntegrationTests
         assert_equal(e[:is_blocked_entitlement], real[i].is_blocked_entitlement) if e.key?(:is_blocked_entitlement)
         assert_equal(e[:service_name], real[i].service_name) if e.key?(:service_name)
         assert_equal(e[:service_state_name], real[i].service_state_name) if e.key?(:service_state_name)
+      end
+    end
+
+    def assert_equal_dates(expected, actual, account_time_zone = nil)
+      if actual.nil?
+        assert_nil(expected)
+      elsif account_time_zone
+        assert_equal(expected, TZInfo::Timezone.get(account_time_zone).utc_to_local(Time.parse(actual)).to_date.to_s)
+      else
+        assert_equal(expected, actual.partition('T')[0])
       end
     end
   end
