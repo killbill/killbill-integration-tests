53d274f9014cb4b7d214b6d5ed227506002600bd
Pierre-Alexandre Meyer <pierre@mouraf.org>
2022-05-25 16:17:51 +0100

    plugins: migrate legacy CyberSource tests to Stripe
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

---
+++ killbill-integration-tests/plugins/stripe/stripe_base.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path('../..', __dir__)
+$LOAD_PATH.unshift File.expand_path('..', __dir__)
+
+require 'plugin_base'
+
+module KillBillIntegrationTests
+  class StripeBase < KillBillIntegrationTests::PluginBase
+    PLUGIN_KEY = 'stripe'
+    PLUGIN_NAME = 'killbill-stripe'
+    # Default to latest
+    PLUGIN_VERSION = nil
+
+    PLUGIN_PROPS = [{ key: 'pluginArtifactId', value: 'stripe-plugin' },
+                    { key: 'pluginGroupId', value: 'org.kill-bill.billing.plugin.java' },
+                    { key: 'pluginType', value: 'java' }].freeze
+
+    PLUGIN_CONFIGURATION = "org.killbill.billing.plugin.stripe.apiKey=#{ENV['STRIPE_API_KEY']}" + "\n" \
+                           "org.killbill.billing.plugin.stripe.publicKey=#{ENV['STRIPE_PUBLIC_KEY']}"
+
+    KILLBILL_STRIPE_PREFIX = '/plugins/killbill-stripe'
+
+    def setup
+      @user = 'Stripe test plugin'
+      @plugins_info = setup_plugin_base('2020-05-01', PLUGIN_KEY, PLUGIN_VERSION, PLUGIN_PROPS)
+      set_configuration(PLUGIN_NAME, PLUGIN_CONFIGURATION)
+    end
+
+    def teardown
+      teardown_plugin_base(PLUGIN_KEY)
+    end
+  end
+end
--- killbill-integration-tests/plugins/stripe/test_stripe.rb
+++ killbill-integration-tests/plugins/stripe/test_stripe.rb
@@ -1,36 +1,11 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path('../..', __dir__)
-$LOAD_PATH.unshift File.expand_path('..', __dir__)
+$LOAD_PATH.unshift File.expand_path('.', __dir__)
 
-require 'plugin_base'
+require 'stripe_base'
 
 module KillBillIntegrationTests
-  class TestStripe < KillBillIntegrationTests::PluginBase
-    PLUGIN_KEY = 'stripe'
-    PLUGIN_NAME = 'killbill-stripe'
-    # Default to latest
-    PLUGIN_VERSION = nil
-
-    PLUGIN_PROPS = [{ key: 'pluginArtifactId', value: 'stripe-plugin' },
-                    { key: 'pluginGroupId', value: 'org.kill-bill.billing.plugin.java' },
-                    { key: 'pluginType', value: 'java' }].freeze
-
-    PLUGIN_CONFIGURATION = "org.killbill.billing.plugin.stripe.apiKey=#{ENV['STRIPE_API_KEY']}" + "\n" \
-                           "org.killbill.billing.plugin.stripe.publicKey=#{ENV['STRIPE_PUBLIC_KEY']}"
-
-    KILLBILL_STRIPE_PREFIX = '/plugins/killbill-stripe'
-
-    def setup
-      @user = 'Stripe test plugin'
-      setup_plugin_base('2020-05-01', PLUGIN_KEY, PLUGIN_VERSION, PLUGIN_PROPS)
-      set_configuration(PLUGIN_NAME, PLUGIN_CONFIGURATION)
-    end
-
-    def teardown
-      teardown_plugin_base(PLUGIN_KEY)
-    end
-
+  class TestStripe < KillBillIntegrationTests::StripeBase
     def test_healthcheck
       healthcheck = if ENV['STRIPE_API_KEY'].nil?
                       JSON.parse(KillBillClient::API.get("#{KILLBILL_STRIPE_PREFIX}/healthcheck", {}, {}).body)
